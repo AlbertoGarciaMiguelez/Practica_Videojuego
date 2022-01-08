86cc8bda6b56a494b4147dd992d74e420e6909ad
Elías Rey Morón <eliasrey12@gmail.com>
2022-01-08 22:33:51 +0100

    Solucionado el problema de las colisiones de los enemigos: Se ha recurrido a la herramienta NavMesh de Unity #35

--- Assets/Scripts/EnemyIA.cs
+++ Assets/Scripts/EnemyIA.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyIA : MonoBehaviour
 {
@@ -8,22 +9,28 @@ public class EnemyIA : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movement;
     public float moveSpeed = 3f;
+    NavMeshAgent agent;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         processInput();
+        agent.SetDestination(player.position);
 
     }
 
     private void FixedUpdate() {
-        moveEnemy();
+        //moveEnemy();
     }
 
 
